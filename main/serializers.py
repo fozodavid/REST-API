df64e12c9a0548c9fc09f822c511f420b7b929db
David Fozo <fozo.david@gmail.com>
2016-12-12 16:54:33 +0100

    Able to save id to the db

--- main/serializers.py
+++ main/serializers.py
@@ -3,7 +3,7 @@ from .models import Event
 
 
 class EventSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField(required=False, allow_blank=True)
     start = serializers.DateTimeField(required=True)
     end = serializers.DateTimeField(required=True)
     label = serializers.CharField(
